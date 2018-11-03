89d0e542034557cfbaf6acb01551d94625c8393a
Philip Winnington <ftcpjw@yahoo.com>
2018-11-03 16:56:06 +0000

    added user create,delete, and update functions

--- src/hr/cli.py
+++ src/hr/cli.py
@@ -2,6 +2,6 @@ import argparse
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parsr.add_agrument('--path','-p',help='the path to the inventory file (JSON)')
-    parser.add_agrument('--export', action='store_true', help='export current setting to inventory file')
+    parser.add_argument('--path','-p',help='the path to the inventory file (JSON)')
+    parser.add_argument('--export', action='store_true', help='export current setting to inventory file')
     return parser
